1caa7d74e72acd8be8e64d3c934b7ade13168d86
shaniaesguerra <shaniae47@gmail.com>
2026-03-05 11:21:00 -0700

    W01 activity express and routing completed

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express'; //import EXPRESS library
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -6,13 +8,39 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+// fileURLToPath = converts that URL to a file system path
+// import.meta.url = gives the URL of the current module
+const __dirname = path.dirname(__filename); //extracts just the directory portion
+
 const app = express();
 
+/**
+  * Configure Express middleware
+  */
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Tells Express that any file in your public directory
+//    should be accessible directly through your website
+
+
+/**
+  * Routes
+  */
 //Route handler for GET results to the root URL ("/"):
 // req = parameter represents the incoming requests
 // res = parameter used to send a response back to clients
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/organizations', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/organizations.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/projects.html'));
 });
 
 //Start the server and make it listen for incoming requests on the
